0ebf9d85147d2e9b3079dcc1d7ba7095fb11e4e9
Rui Martins <Mr.Rui.Martins@gmail.com>
2015-03-19 10:17:06 +0100

    name of client
    changed the name

--- DatabaseManagementSystem/DatabaseManagementSystem/Client.cs
+++ DatabaseManagementSystem/DatabaseManagementSystem/Client.cs
@@ -11,7 +11,7 @@ namespace DatabaseManagementSystem
 		public Client(string nameOfClient, string identifier)
 		{
 			this.ID = identifier;
-			this.name = name;
+            this.name = nameOfClient;
 		}
 	}
 }
