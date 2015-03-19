49f145f60a22e33156478d0f49ccc10a35a4716d
Cebrail Erdogan <cebonet@msn.com>
2015-03-19 10:03:09 +0100

    Classes

---
+++ DatabaseManagementSystem/DatabaseManagementSystem/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DatabaseManagementSystem
+{
+	public class Client
+	{
+		String name;
+		string ID;
+
+		//Constructor
+		public Client(String nameOfClient, int identifier)
+		{
+			this.ID = identifier;
+			this.name = name;
+		}
+	}
+}
+
---
+++ DatabaseManagementSystem/DatabaseManagementSystem/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DatabaseManagementSystem
+{
+	public class Transaction
+	{
+		Client sender,reciever;
+		string transID;
+
+		public Transaction(Client sender,Client reciever)
+		{
+			this.sender = sender;
+			this.reciever = reciever;
+		}
+	}
+}
+
