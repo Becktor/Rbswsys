d7257fd4dec645c84a4b0009579142f65f31e999
Rui Martins <Mr.Rui.Martins@gmail.com>
2015-03-19 10:06:05 +0100

    reading some user input for the transactions
    fooling arround with C# to get comfortable with the language :+1:

--- DatabaseManagementSystem/DatabaseManagementSystem/Program.cs
+++ DatabaseManagementSystem/DatabaseManagementSystem/Program.cs
@@ -10,9 +10,32 @@ namespace DatabaseManagementSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            string message = "Hello, welcome to the Transaction Analyser!";
+            bool is_valid_graph = false;
+            string user_name;
+            string password;
+            string user_input = "";
+
+            if (!is_valid_graph) {
+
+                Console.WriteLine(message);
+ 
+                // what a nice little comment here :)
+                Console.WriteLine("Press any key to continue:");
+                Console.ReadKey();
+                Console.WriteLine(Environment.NewLine + "Press enter your username: ");
+                user_name = Console.ReadLine();
+                Console.WriteLine("Press enter your password: ");
+                password = Console.ReadLine();
+
+                Console.WriteLine("Welcome, " + user_name + "! Please enter your transactions: (enter \"exit\" to quit)");
+                while (user_input != "exit")
+                {
+                    user_input = Console.ReadLine();
+                }
+
+            }
+
         }
     }
 }
