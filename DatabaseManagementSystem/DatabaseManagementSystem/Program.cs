a6d85be9d26d7c0220e1187f7e38701100b948ce
Rui Martins <Mr.Rui.Martins@gmail.com>
2015-03-19 09:26:51 +0100

    a nice hello world for you all.
    hat hat hat hat hat hat

--- DatabaseManagementSystem/DatabaseManagementSystem/Program.cs
+++ DatabaseManagementSystem/DatabaseManagementSystem/Program.cs
@@ -10,6 +10,9 @@ namespace DatabaseManagementSystem
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
