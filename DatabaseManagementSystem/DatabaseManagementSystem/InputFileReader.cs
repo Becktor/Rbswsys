ae10dcc3317d1c2f3215270c187e96aff37f734b
davidsoendoro <david.soendoro@gmail.com>
2015-04-13 21:36:23 +0200

    TransactionGraphs addition
    Addition class ´TransactionGraph

--- DatabaseManagementSystem/DatabaseManagementSystem/InputFileReader.cs
+++ DatabaseManagementSystem/DatabaseManagementSystem/InputFileReader.cs
@@ -19,12 +19,22 @@ namespace DatabaseManagementSystem
                 user_input = Console.ReadLine();
                 if (user_input.Equals("draw"))
                 {
+                    doDrawing();
                     // Thread drawingThread = new Thread(drawGraph) {IsBackground = true};
                     // drawingThread.Start();
-                    GraphGenerator graph = new GraphGenerator();
-                    graph.drawGraph();
+                    //GraphGenerator graph = new GraphGenerator();
+                    //graph.drawGraph();
                 }
             }
+        }
+
+        private void doDrawing()
+        {
+            TransactionGraph transactionGraph = new TransactionGraph();
+
+            transactionGraph.addDependency("T1", null);
+
+            transactionGraph.drawGraph();
         }
 	}
 }
