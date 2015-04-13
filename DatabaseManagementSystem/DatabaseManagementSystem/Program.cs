7f575ba45e3e9cc9898a08328080c404dd1fdfa1
Rui Martins <Mr.Rui.Martins@gmail.com>
2015-04-13 18:02:12 +0200

    david rocks

--- DatabaseManagementSystem/DatabaseManagementSystem/Program.cs
+++ DatabaseManagementSystem/DatabaseManagementSystem/Program.cs
@@ -22,6 +22,7 @@ namespace DatabaseManagementSystem
             Console.WriteLine(message);
 
             reader.readInput();
+            // David rocks
         }
 
         private static int Divide(int p1, int p2)
